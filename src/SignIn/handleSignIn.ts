4433bed9fde274eb7c7dcaf1ab120bc5368c199f
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-04 14:26:36 +0530

    integrate: integrate the services in the sign in and sign up functionality

--- src/SignIn/handleSignIn.ts
+++ src/SignIn/handleSignIn.ts
@@ -1,6 +1,6 @@
 import customAlert from "../../node_modules/@pranshupatel/custom-alert/script";
 import { User } from "../SignUp/commonTypeInterface.js";
-import { getUserInfo } from "../Services/getRequest.js";
+import { GET } from "../Services/Service.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initializeEventListener();
@@ -17,28 +17,27 @@ function initializeEventListener() {
 }
 
 async function handleSignIn() {
-  let username = (document.getElementById("email") as HTMLInputElement).value;
-  let password = (document.getElementById("password") as HTMLInputElement)
+  const username = (document.getElementById("email") as HTMLInputElement).value;
+  const password = (document.getElementById("password") as HTMLInputElement)
     .value;
+  try {
+    const userData = (await GET("user")) as User[];
 
-  let userData = await getUserInfo();
-  let conditionalFlag: number = 0;
+    let userObject = userData.find(
+      (user: User) => user.email === username && user.password === password
+    );
 
-  userData.forEach((user: User) => {
-    if (user.email === username) {
-      if (user.password === password) {
-        conditionalFlag = 1;
-        customAlert("success", "top-right", "Login successful");
-        localStorage.setItem("current-user-id", user.userId);
-        setTimeout(() => {
-          if (user.role === "buyer") document.location.href = "#";
-          else document.location.href = "#";
-        }, 1000);
-      }
+    if (userObject) {
+      customAlert("success", "top-right", "Login successful");
+      localStorage.setItem("user-token", userObject.userId);
+      setTimeout(() => {
+        if (userObject.role === "buyer") document.location.href = "#";
+        else document.location.href = "#";
+      }, 1000);
+    } else {
+      customAlert("error", "top-right", "Invalid username and password");
     }
-  });
-
-  if (conditionalFlag === 0) {
-    customAlert("error", "top-right", "Invalid username and password");
+  } catch (err) {
+    console.log(err);
   }
 }
