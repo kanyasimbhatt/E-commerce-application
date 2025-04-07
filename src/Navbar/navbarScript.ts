363bd8e744fc65d57e043c90f3d4d01989a261bb
Kanya Bhatt <kanya.bhatt@simformsolutions.com>
2025-04-07 19:17:45 +0530

    modify: modify the navbar code to reduce redundency

--- src/Navbar/navbarScript.ts
+++ src/Navbar/navbarScript.ts
@@ -1,11 +1,11 @@
-function induceNavbarCodeForBuyer(element: HTMLElement) {
+function induceNavbarCode(element: HTMLElement, pageType: string) {
   element.innerHTML = `
       <!-- Logo  -->
       <a
         class="navbar-brand d-flex gap-2 align-items-center justify-content-center"
-        href="./viewAllProducts.html"
+        href=""
       >
-        <img src="e-commerce-application/src/Utility/images/logo.png" alt="Logo" height="40" width="40" />
+        <img src=""https://th.bing.com/th/id/OIP.jZz_UxnqxGF3Fa2U3mXQpAHaHa?rs=1&pid=ImgDetMain"" alt="Logo" height="40" width="40" />
         <h3 class="text-primary">Shopify</h3>
       </a>
       <div class="ms-auto d-flex align-items-center gap-4">
@@ -38,70 +38,38 @@ function induceNavbarCodeForBuyer(element: HTMLElement) {
         </a>
       </div>
    `;
+  let sellerFlag = 0;
+  if (pageType === "seller") {
+    sellerFlag = 1;
+    document.querySelectorAll(".remove").forEach((element) => {
+      (element as HTMLElement).style.display = "none";
+      element.classList.remove("profile-user-info-buyer");
+      element.classList.add("profile-user-info-seller");
+    });
+  }
 
   const profileElement = document.getElementsByClassName(
     "profile-popup"
   )[0] as HTMLElement;
 
   profileElement.addEventListener("click", () => {
-    console.log("hello");
-
-    document
-      .getElementsByClassName("profile-user-info-buyer")[0]
-      .classList.toggle("selected");
-  });
-}
-
-function induceNavbarCodeForSeller(element: HTMLElement) {
-  element.innerHTML = `
-
-      <!-- Logo  -->
-      <a
-        class="navbar-brand d-flex gap-2 align-items-center justify-content-center"
-        href="./viewAllProducts.html"
-      >
-        <img src="e-commerce-application/src/Utility/images/logo.png" alt="Logo" height="40" width="40" />
-        <h3 class="text-primary">Shopify</h3>
-      </a>
-      <div class="ms-auto d-flex align-items-center gap-4">
-        <!-- Search bar -->
-
-        <div
-          class="d-flex flex-row justify-content-center align-items-center border rounded px-2 me-3 remove"
-        >
-          <i class="fa fa-search px-2"></i>
-          <input
-            type="text"
-            class="form-control bg-light border-0"
-            placeholder="Search products..."
-            aria-label="Search products"
-          />
-        </div>
-
-        <!-- user-info -->
-        <div class="profile-logo-info-wrapper">
-          <a href="#" class="me-3 profile-popup">
-            <i class="bi bi-person-circle" style="font-size: 1.7rem"></i>
-          </a>
-          <div class="profile-user-info-seller"></div>
-        </div>
-      </div>`;
-  const profileElement = document.getElementsByClassName(
-    "profile-popup"
-  )[0] as HTMLElement;
-
-  profileElement.addEventListener("click", () => {
-    document
-      .getElementsByClassName("profile-user-info-seller")[0]
-      .classList.toggle("selected");
+    if (sellerFlag === 0) {
+      document
+        .getElementsByClassName("profile-user-info-buyer")[0]
+        .classList.toggle("selected");
+    } else {
+      document
+        .getElementsByClassName("profile-user-info-seller")[0]
+        .classList.toggle("selected");
+    }
   });
 }
 
 export function redirectNavbarRequest(element: HTMLElement) {
   const currentLink: string = document.location.href;
   if (currentLink.includes("Buyer")) {
-    induceNavbarCodeForBuyer(element);
+    induceNavbarCode(element, "buyer");
   } else if (currentLink.includes("Seller")) {
-    induceNavbarCodeForSeller(element);
+    induceNavbarCode(element, "seller");
   }
 }
