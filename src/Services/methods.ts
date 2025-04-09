92bdfb2e35ec4e703a2489b09135cacc2d381570
Hetal Desai <hetal.desai@simformsolutions.com>
2025-04-09 11:18:53 +0530

    Remove: Remove console.log from methods.ts

--- src/Services/methods.ts
+++ src/Services/methods.ts
@@ -1,7 +1,6 @@
 import { FETCH_URL } from "../SignUp/constants";
 export async function GET<T>(url: string, options: object = {}): Promise<T> {
   const response = await fetch(`${FETCH_URL}/${url}`);
-  console.log(response);
 
   if (!response.ok) {
     throw new Error(`Couldn't fetch data`);
